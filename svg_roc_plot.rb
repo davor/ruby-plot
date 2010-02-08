9962d9797881d14969ef91e16541e554e7d93926
David Vorgrimmler <vorgrimmlerdavid@gmx.de>
2010-02-08 14:19:54 +0100

    Some additional tests and other changes.

--- svg_roc_plot.rb
+++ svg_roc_plot.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/ruby1.8
 # svg_roc_plot program - svg_roc_plot.rb
 #       
-#       Copyright 2010 vorgrimmler <dv@fdm.uni-freiburg.de>
-#       
-
+#	Copyright 2010 vorgrimmler <dv(a_t)fdm.uni-freiburg.de>
+#	This ruby program exports inputs data(from true-positive-rate and false-positive-rate files) to a *.svg file using gnuplot. Depending on the amount of input data is possible to create 1 to n curves in one plot. 
+#	Gnuplot is needed. Please install befor using svg_roc_plot. "sudo apt-get install gnuplot" (on debian systems).
+#	Usage: See below.
 
 # Main
 STDOUT.sync = true
@@ -20,24 +21,34 @@ if input_number%2 != 0  || $*.size<=1
 end
 
 if status
-    puts "Usage: #{$0} ([filename_1] [filename_2]) ... ([filename_1] [filename_2])" # [filename_3].actives [filename_4].inactives [filename_5].smi"
+    puts "Usage: #{$0} ([filename_1] [filename_2]) ... ([filename_1] [filename_2])"
     puts "       cmd=filename_1 : This should be the filename of the ture-positiv-rate data."
     puts "       cmd=filename_2 : This should be the filename of the false-positiv-rate data."
 	puts "       Only pairs of input files are allowed but at least one."
+	puts "       Each input file has to provide one float/int number from 0 to 100 per line."
     exit
+end
+
+# gnuplot check
+gnuplot=`which gnuplot | grep -o gnuplot`
+if gnuplot == "gnuplot\n"
+	puts "Gnuplot installed."
+else
+	puts "Please install gnuplot."
+	puts "sudo apt-get install gnuplot"
+	exit
 end
 
 dat_number=0
 # -----------------------------------------------------
-# create *0.dat file for gnuplot
+# create random_0.dat file for gnuplot
 # -----------------------------------------------------
-# write true/false arrays to one array
 output_dat_arr = Array.new
 for i in 0..100
 	output_dat_arr[i] = "#{i} #{i}"
 end
 # -----------------------------------------------------
-# write *0.dat files
+# write random_0.dat files
 # -----------------------------------------------------
 # write output_dat_arr content in new *.dat file
 File.open( "random_#{dat_number}.dat", "w" ) do |the_file|
@@ -46,7 +57,15 @@ end
 puts "random_#{dat_number}.dat created."
 output_dat_arr.clear
 
+# float check
+def numeric?(object)
+  true if Float(object) rescue false
+end
 
+# -----------------------------------------------------
+# create *.dat files of imported data for gnuplot
+# -----------------------------------------------------
+# write true/false arrays to one array
 for i in 0..input_number/2-1
 	
 	# set paths
@@ -55,37 +74,46 @@ for i in 0..input_number/2-1
 	dat_number += 1
 	
 	# check files
-	true_pos_check = File.new(true_pos_path, "r")
-	false_pos_check = File.new(false_pos_path, "r")
-	
-	
-	if true_pos_check && false_pos_check
+	if File.exist?(true_pos_path) && File.exist?(false_pos_path)
 		# read files to array
 		true_pos_arr = IO.readlines(true_pos_path)
 		false_pos_arr = IO.readlines(false_pos_path)
-		
-		# check length of input files
-		if true_pos_arr.length == false_pos_arr.length
-			#puts "Same length!"
-			for i in 0..true_pos_arr.length-1
-				output_dat_arr[i] = " #{true_pos_arr[i].chop} #{false_pos_arr[i]}"
-			end
 			
-			# -----------------------------------------------------
-			# write *.dat files
-			# -----------------------------------------------------
-			# write output_dat_arr content in new *.dat file
-			File.open( "#{$*[dat_number*2-2]}_#{dat_number}.dat", "w" ) do |the_file|
-			   	the_file.puts output_dat_arr
+			# check length of input files
+			if true_pos_arr.length == false_pos_arr.length
+				#puts "Same length!"
+				for i in 0..true_pos_arr.length-1
+					# check if array entries are float format and between 0.0 and 100.0
+					if numeric?(true_pos_arr[i].tr(',', '.')) && true_pos_arr[i].tr(',', '.').to_f <= 100 && true_pos_arr[i].tr(',', '.').to_f >= 0
+						if  numeric?(false_pos_arr[i].tr(',', '.')) && false_pos_arr[i].tr(',', '.').to_f <= 100 && false_pos_arr[i].tr(',', '.').to_f >= 0
+							output_dat_arr[i] = " #{true_pos_arr[i].chop} #{false_pos_arr[i]}"
+						else
+							puts "The following file has not the right format #{false_pos_path} in line ##{i+1}"
+							puts "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
+							exit
+						end
+					else
+						puts "The following file has not the right format: #{true_pos_path} in line ##{i+1}"
+						puts "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
+						exit
+					end
+				end
+				
+				# -----------------------------------------------------
+				# write *.dat files
+				# -----------------------------------------------------
+				# write output_dat_arr content in new *.dat file
+				File.open( "#{$*[dat_number*2-2]}_#{dat_number}.dat", "w" ) do |the_file|
+				   	the_file.puts output_dat_arr
+				end
+				puts "#{$*[dat_number*2-2]}_#{dat_number}.dat created."
+				output_dat_arr.clear
+						
+			else
+				puts "Input file pair(#{$*[dat_number*2-2]}, #{$*[dat_number*2-1]}) have no the same number of elements."
+				exit
 			end
-			puts "#{$*[dat_number*2-2]}_#{dat_number}.dat created."
-			output_dat_arr.clear
-					
-		else
-			puts "Input file pair(#{$*[dat_number*2-2]}, #{$*[dat_number*2-1]}) have no the same number of elements."
-			exit
-		end
-	
+		
 	else
 		puts "Unable to open one or more input file!"
 		exit
@@ -134,6 +162,7 @@ File.open( "result.plt", "w" ) do |the_file|
 end
 puts "result.plt created."
 
+# start gnuplot with created *.plt file
 `gnuplot result.plt`
 
 puts "result.svg created."
