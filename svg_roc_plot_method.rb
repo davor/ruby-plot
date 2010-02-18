caf67bd51c4c8adf83689f564ab8d79c037b7797
David Vorgrimmler <vorgrimmlerdavid@gmx.de>
2010-02-18 13:18:22 +0100

    merge (by hand)

--- svg_roc_plot_method.rb
+++ svg_roc_plot_method.rb
@@ -6,7 +6,15 @@
 #	Gnuplot is needed. Please install befor using svg_roc_plot. "sudo apt-get install gnuplot" (on debian systems).
 #	Usage: See below.
 
-def svg_roc_plot(svg_path, title, x_lable, y_lable, names, *data)
+require 'logger'
+
+unless(defined? LOGGER)
+  LOGGER = Logger.new(STDOUT)
+  LOGGER.datetime_format = "%Y-%m-%d %H:%M:%S "
+end
+
+module Svg_Roc_Plot
+ def self.plot(svg_path, title, x_lable, y_lable, names, *data)
 	# Main
 	STDOUT.sync = true
 	# -----------------------------------------------------
@@ -14,28 +22,26 @@ def svg_roc_plot(svg_path, title, x_lable, y_lable, names, *data)
 	# -----------------------------------------------------
 	# check parameters
 	status=false
-	puts "#{names.length} algs entered"
+  	LOGGER.debug "#{names.length} algs entered"
 	
 	if names.length != data.length/2
 	    status=true
 	end
 	
 	if status
-	    puts "Usage: svg_roc_plot (svg_path(?), title(string), x-lable(string), y-lable(sting), algorithms(array), true_pos_data1(array), false_pos_data1(array), ...,  true_pos_data_n(array), false_pos_data_n(array))"
-		puts "       Only pairs of data are allowed but at least one."
-		puts "       Each data array has to provide one float/int number from 0 to 100 per entry."
-	    exit
-	end
+    raise "Usage: svg_roc_plot (svg_path(?), title(string), x-lable(string), y-lable(sting), algorithms(array), true_pos_data1(array), false_pos_data1(array), ...,  true_pos_data_n(array), false_pos_data_n(array))\n"+
+  		      "       Only pairs of data are allowed but at least one.\n"+
+  		      "       Each data array has to provide one float/int number from 0 to 100 per entry."
+  	end
 	
 	# gnuplot check
 	gnuplot=`which gnuplot | grep -o gnuplot`
 	if gnuplot == "gnuplot\n"
-		puts "Gnuplot installed."
-	else
-		puts "Please install gnuplot."
-		puts "sudo apt-get install gnuplot"
-		exit
-	end
+  		LOGGER.debug "Gnuplot is already installed."
+  	else
+  		raise "Please install gnuplot.\n"+
+  		      "sudo apt-get install gnuplot"
+  	end
 	
 	dat_number=0
 	# -----------------------------------------------------
@@ -52,119 +58,117 @@ def svg_roc_plot(svg_path, title, x_lable, y_lable, names, *data)
 	File.open( "random_#{dat_number}.dat", "w" ) do |the_file|
 	   	the_file.puts output_dat_arr
 	end
-	puts "random_#{dat_number}.dat created."
-	output_dat_arr.clear
+  	LOGGER.debug "random_#{dat_number}.dat created."
+ output_dat_arr.clear
 	
-	# float check
-	def numeric?(object)
-	  true if Float(object) rescue false
-	end
-	
-	# -----------------------------------------------------
-	# create *.dat files of imported data for gnuplot
-	# -----------------------------------------------------
-	# write true/false arrays to one array
-	for i in 0..names.length-1#/2-1
-		true_pos_arr = data[i*2]
-		false_pos_arr = data[i*2+1]
-		#check length of input files
-		if true_pos_arr.length == false_pos_arr.length
-			#puts "Same length!"
-			for j in 0..true_pos_arr.length-1
-				#check if array entries are float format and between 0.0 and 100.0
-				if numeric?(true_pos_arr[j].to_s.tr(',', '.')) && true_pos_arr[j].to_s.tr(',', '.').to_f <= 100 && true_pos_arr[j].to_s.tr(',', '.').to_f >= 0
-					if  numeric?(false_pos_arr[j].to_s.tr(',', '.')) && false_pos_arr[j].to_s.tr(',', '.').to_f <= 100 && false_pos_arr[j].to_s.tr(',', '.').to_f >= 0
-						output_dat_arr[j] = "#{true_pos_arr[j]} #{false_pos_arr[j]}"
-					else
-						puts "The data of #{names[i]}  has not the right formatin at position #{j}"
-						puts "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
-						exit
-					end
-				else
-					puts "The data of #{names[i]}  has not the right formatin at position #{j}"
-					puts "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
-					exit
-				end
-			end
-			
-			#-----------------------------------------------------
-			#write *.dat files
-			#-----------------------------------------------------
-			#write output_dat_arr content in new *.dat file
-			File.open( "#{names[i]}.dat", "w" ) do |the_file|
-			   	the_file.puts output_dat_arr
-			end
-			puts "#{names[i]}.dat created."
-			output_dat_arr.clear
-					
-		else
-			puts "Data pair of #{names[i]} have no the same number of elements."
-			exit
-		end
-	end
-	
-	# -----------------------------------------------------
-	# create *.plt file for gnuplot
-	# -----------------------------------------------------
-	# 
-	output_plt_arr = Array.new
-	output_plt_arr.push "# Specifies encoding and output format"
-	output_plt_arr.push "set encoding default"
-	output_plt_arr.push "set terminal svg"
-	output_plt_arr.push "set output '#{svg_path}'"
-	output_plt_arr.push ""
-	output_plt_arr.push "# Specifies the range of the axes and appearance"
-	
-	output_plt_arr.push "set xrange [0:100]"
-	output_plt_arr.push "set yrange [0:100]"
-	output_plt_arr.push "set grid"
-	output_plt_arr.push "set title \"#{title}\""
-	output_plt_arr.push "set key invert reverse Left outside"
-	output_plt_arr.push "set xlabel \"#{x_lable}\""
-	output_plt_arr.push "set ylabel \"#{y_lable}\""
-	output_plt_arr.push "set arrow from 0,0 to 100,100 nohead"
-	output_plt_arr.push ""
-	output_plt_arr.push ""
-	output_plt_arr.push ""
-	output_plt_arr.push ""
-	output_plt_arr.push "# Draws the plot and specifies its appearance ..."
-	output_plt_arr.push "plot	\\"#'random_0.dat' using 1:2 title 'random' with lines, \\"
-	i = 0
-	for i in 0..names.length-1
-		if i == names.length-1
-			output_plt_arr.push "	'#{names[i]}.dat'  using 2:1 title '#{names[i]}' with lines"
-		else
-			output_plt_arr.push "	'#{names[i]}.dat'  using 2:1 title '#{names[i]}' with lines, \\"
-		end
-	end
-	output_plt_arr.push ""
-	output_plt_arr.push ""
-	
-	# -----------------------------------------------------
-	# write *.plt files
-	# -----------------------------------------------------
-	# write output_dat_arr content in new *.dat file
-	File.open( "config.plt", "w" ) do |the_file|
-		the_file.puts output_plt_arr
-	end
-	puts "config.plt created."
-	
-	# start gnuplot with created *.plt file
-	`gnuplot config.plt`
-	puts "#{svg_path} created."
-	
-	# -----------------------------------------------------
-	# remove *.plt and *.dat files
-	# -----------------------------------------------------
-	`rm config.plt`
-	puts "config.plt removed."
-	`rm random_0.dat`
-	puts "random_0.dat removed."
-	for i in 0..names.length-1
-		`rm #{names[i]}.dat`
-		puts "#{names[i]}.dat removed."
-	end
+    # -----------------------------------------------------
+    # create *.dat files of imported data for gnuplot
+    # -----------------------------------------------------
+# write true/false arrays to one array
+for i in 0..names.length-1#/2-1
+    true_pos_arr = data[i*2]
+    false_pos_arr = data[i*2+1]
+    #check length of input files
+    if true_pos_arr.length == false_pos_arr.length
+        #LOGGER.debug "Same length!"
+        for j in 0..true_pos_arr.length-1
+            #check if array entries are float format and between 0.0 and 100.0
+            if numeric?(true_pos_arr[j].to_s.tr(',', '.')) && true_pos_arr[j].to_s.tr(',', '.').to_f <= 100 && true_pos_arr[j].to_s.tr(',', '.').to_f >= 0
+                if  numeric?(false_pos_arr[j].to_s.tr(',', '.')) && false_pos_arr[j].to_s.tr(',', '.').to_f <= 100 && false_pos_arr[j].to_s.tr(',', '.').to_f >= 0
+                    output_dat_arr[j] = "#{true_pos_arr[j]} #{false_pos_arr[j]}"
+                else
+                raise "The data of #{names[i]}  has not the right formatin at position #{j}\n"+
+                      "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
+            end
+        else
+        raise "The data of #{names[i]}  has not the right formatin at position #{j}+\n"
+               "The right format is one float/int from 0 to 100 each line (e.g. '0'; '23,34'; '65.87' or '99')"
+    end
+end
+        #-----------------------------------------------------
+        #write *.dat files
+        #-----------------------------------------------------
+        #write output_dat_arr content in new *.dat file
+        File.open( "#{names[i]}.dat", "w" ) do |the_file|
+            the_file.puts output_dat_arr
+        end
+        LOGGER.debug "#{names[i]}.dat created."
+        output_dat_arr.clear
+                
+    else
+        raise "Data pair of #{names[i]} have no the same number of elements."
+    end
 end
 
+# -----------------------------------------------------
+# create *.plt file for gnuplot
+# -----------------------------------------------------
+# 
+output_plt_arr = Array.new
+output_plt_arr.push "# Specifies encoding and output format"
+output_plt_arr.push "set encoding default"
+output_plt_arr.push "set terminal svg"
+output_plt_arr.push "set output '#{svg_path}'"
+output_plt_arr.push ""
+output_plt_arr.push "# Specifies the range of the axes and appearance"
+
+output_plt_arr.push "set xrange [0:100]"
+output_plt_arr.push "set yrange [0:100]"
+output_plt_arr.push "set grid"
+output_plt_arr.push "set title \"#{title}\""
+output_plt_arr.push "set key invert reverse Left outside"
+output_plt_arr.push "set xlabel \"#{x_lable}\""
+output_plt_arr.push "set ylabel \"#{y_lable}\""
+output_plt_arr.push "set arrow from 0,0 to 100,100 nohead"
+output_plt_arr.push ""
+output_plt_arr.push ""
+output_plt_arr.push ""
+output_plt_arr.push "# Draws the plot and specifies its appearance ..."
+output_plt_arr.push "plot	\\"#'random_0.dat' using 1:2 title 'random' with lines, \\"
+i = 0
+for i in 0..names.length-1
+    if i == names.length-1
+        output_plt_arr.push "	'#{names[i]}.dat'  using 2:1 title '#{names[i]}' with lines"
+    else
+        output_plt_arr.push "	'#{names[i]}.dat'  using 2:1 title '#{names[i]}' with lines, \\"
+    end
+end
+output_plt_arr.push ""
+output_plt_arr.push ""
+
+# -----------------------------------------------------
+# write *.plt files
+# -----------------------------------------------------
+# write output_dat_arr content in new *.dat file
+File.open( "config.plt", "w" ) do |the_file|
+    the_file.puts output_plt_arr
+end
+LOGGER.debug "config.plt created."
+
+# start gnuplot with created *.plt file
+`gnuplot config.plt`
+LOGGER.debug "#{svg_path} created."
+
+# -----------------------------------------------------
+# remove *.plt and *.dat files
+# -----------------------------------------------------
+`rm config.plt`
+LOGGER.debug "config.plt removed."
+`rm random_0.dat`
+LOGGER.debug "random_0.dat removed."
+for i in 0..names.length-1
+    `rm #{names[i]}.dat`
+    LOGGER.debug "#{names[i]}.dat removed."
+end
+end
+ 
+  private
+  # float check
+  def self.numeric?(object)
+   true if Float(object) rescue false
+  end
+
+
+end
 #test function
-svg_roc_plot("/media/sda4/baloerrach/work/guetlein/svg_roc_plot/result.svg" , "name of title", "pos", "neg", ["name1", "name2", "name3"], [20,60,80], [15,50,90], [20,40,80], [15,30,90], [20,30,40,80], [15,20,50,90])
+#Svg_Roc_Plot::plot("/home/martin/tmp/result.svg" , i"name of title", "pos", "neg", ["name1", "name2"], [20,60,80], [15,50,90],[10,25,70,95],[20,40,50,70])
